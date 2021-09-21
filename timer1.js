7c0c62d114d4140eca51d8dce37dde07b6f62f4c
Ralenac <t.ralenac@gmail.com>
2021-09-21 18:26:37 -0400

    Creating Timer with setTimeout

---
+++ timer1.js
@@ -0,0 +1,19 @@
+
+
+//process.stdout.write('\x07'); -> for the beep sound
+
+
+
+
+
+
+const args = process.argv;
+let newArgs = process.argv.slice(2);
+for (let i = 0; i < newArgs.length; i++) {
+  let newTime = 1000 * newArgs[i] 
+  setTimeout(() => {console.log(`${newArgs[i]} seconds`, process.stdout.write('\x07'))
+}, newTime)
+};
+
+//node timer1.js 10 3 5 15 9
+
