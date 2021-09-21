6cf848540ab812d866fae9d5fc7031d9074ceead
Teodora Stefanovic <88411637+Ralenac@users.noreply.github.com>
2021-09-21 18:29:50 -0400

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -10,7 +10,7 @@
 const args = process.argv;
 let newArgs = process.argv.slice(2);
 for (let i = 0; i < newArgs.length; i++) {
-  let newTime = 1000 * newArgs[i] 
+  let newTime = 1000 * newArgs[i] //wee need to get ms (1s=1000ms)
   setTimeout(() => {console.log(`${newArgs[i]} seconds`, process.stdout.write('\x07'))
 }, newTime)
 };
